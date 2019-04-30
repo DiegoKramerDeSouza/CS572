8235e8706dcdda6da2a3c20e6e8cbd89bc9d8b08
Diego Kramer de Souza <diego.kramer99@gmail.com>
2019-04-30 07:39:45 -0500

    Assignment Week 1, Day 1

--- Lab01_W1_D1/FilterWords.js
+++ Lab01_W1_D1/FilterWords.js
@@ -1,16 +1,38 @@
-// Reactive
+// ReactiveX Observable
 const { from } = rxjs;
+
+// Observable
+class Filter{
+    constructor(){
+        setTimeout(_ => this.emit(), 200);
+    }
+    emit(){
+        if(this.oncomplete) this.oncomplete();
+    }
+}
+function observable(observer){
+    let filter = new Filter();
+    filter.oncomplete = _ => observer.complete();
+}
+const sub = observable({
+    complete(){
+        promise().then(data => console.log("(" + data + ") ---> Observable"));
+    }
+});
+
 // Replace listed words using Regular Expressions
 String.prototype.filterWords = function(words){
     const regx = new RegExp(words.join("|"), "gi");
     return this.replace(regx, "***");
 }
+
 // Create a promise resolving filterWords
 const promise = _ => {
     return new Promise(resolve => {
         resolve("This house is nice!".filterWords(["house", "nice"]) + " ---> Promise");
     })
 }
+
 // Create async function asyncFilter to call promise
 async function asyncFilter(){
     try{
@@ -20,7 +42,8 @@ async function asyncFilter(){
         console.error("Promise Error");
     }
 }
+
 console.log("This house is nice!".filterWords(["house", "nice"]));
 promise().then(data => console.log(data));
 asyncFilter(); 
-from(promise()).subscribe(e => console.log("(" + e + ") ---> Observable"));
+from(promise()).subscribe(e => console.log("(" + e + ") ---> ReactiveX Observable"));
