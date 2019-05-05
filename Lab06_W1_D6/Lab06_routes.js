069d0b24f75ff2869480f908346f23a855811739
Diego Kramer de Souza <diego.kramer99@gmail.com>
2019-05-04 20:07:48 -0500

    Assignment Week 1, Day 6

---
+++ Lab06_W1_D6/Lab06_routes.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const router = express.Router();
+const jsonParser = express.json();
+
+let grades = [
+    {id: 'st001', name: 'John', course: 'CS572', grade: '95'},
+    {id: 'st002', name: 'Ross', course: 'CS572', grade: '90'},
+    {id: 'st003', name: 'Ann', course: 'CS572', grade: '93'}
+]
+
+// get all
+router.route('/')
+    .get( jsonParser,
+        function(req, res, next){
+            if(req.query.id) return next('route');
+            console.log(grades);
+            return next();
+        },
+        function(req, res){
+            res.send(grades);
+            res.end();
+        }
+    );
+// get one with query
+router.route('/')
+    .get( jsonParser,
+        function(req, res, next){
+            console.log(grades.filter(grade => grade.id == req.query.id));
+            return next();
+        },
+        function(req, res){
+            res.send(grades.filter(grade => grade.id == req.query.id));
+            res.end();
+        }
+    );
+// get one with params
+router.route('/:id')
+    .get( jsonParser,
+        function(req, res, next){
+            if(req.params){
+                console.log(grades.filter(grade => grade.id == req.params.id));
+                return next();
+            } else {
+                res.status(404).end();
+            }
+        },
+        function(req, res){
+            res.send(grades.filter(grade => grade.id == req.params.id));
+            res.end();
+        }
+    )
+    .delete( jsonParser,
+        function(req, res, next){
+            if(req.params){
+                console.log(grades.filter(grade => grade.id != req.params.id));
+                return next();
+            } else {
+                res.status(404).end();
+            }
+        }, 
+        function(req, res){
+            grades = grades.filter(grade => grade.id != req.params.id)
+            res.send(grades);
+            res.end();
+        }
+    )
+    .put( jsonParser,
+        function(req, res, next){
+            if(req.params.id && req.query){
+                console.log(req.query);
+                return next();
+            } else {
+                res.status(404).end();
+            }
+        },
+        function(req, res){
+            let target = grades.indexOf(grades.find(grade => grade.id == req.params.id));
+            grades[target] = req.query;
+            res.send(grades);
+            res.end();
+        }
+    );
+
+router.route('/')
+    .post( jsonParser,
+        function(req, res, next){
+            if(req.query){
+                grades.push(req.query);
+                return next();
+            } else {
+                res.status(404).end();
+            }
+        },
+        function(req, res){
+            console.log(grades);
+            res.send(grades);
+            res.end();
+        }
+    );
+
+
+module.exports = router;
