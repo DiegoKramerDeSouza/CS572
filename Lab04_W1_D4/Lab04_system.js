c4d09aedca1674c747211ac21d2626c096be6e74
Diego Kramer de Souza <diego.kramer99@gmail.com>
2019-05-06 20:50:50 -0500

    Assignment Week 1, Day 4

--- Lab04_W1_D4/Lab04_system.js
+++ Lab04_W1_D4/Lab04_system.js
@@ -1,16 +1,24 @@
 const os = require('os');
+const {Observable} = require('rxjs');
+
 const systemChecking = function(){
 	// Get Amount of RAM in GB
 	let ram = os.totalmem() / (1024 * 1024 * 1024);
 	ram = Math.round(ram * 100)/100;
 	// Get CPUs
-	let cpus = os.cpus();
-	
+	let cpus = os.cpus().length;
 	return {RAM: ram, CPUS: cpus};
 }
 process.on('message', msg => {
-	const sys = systemChecking();
-	if(sys.RAM < 4) process.send("This app needs at least 4GB of RAM");
-	else if(sys.CPUS < 2) process.send("Processor is not supported");
-	else process.send("System is checked successfully!");
+	const obs$ = new Observable(obs => {
+		const sys = systemChecking();
+		if(sys.RAM < 4) obs.error("This app needs at least 4GB of RAM");
+		if(sys.CPUS < 2) obs.error("Processor is not supported");
+		obs.complete();
+	});
+	obs$.subscribe(
+		null,
+		err => process.send(err),
+		_ => process.send("System is checked successfully!")
+	);
 });
